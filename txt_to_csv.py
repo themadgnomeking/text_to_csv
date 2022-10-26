83bea6fd3ae7706c89ec17d746f5f1f130247cb5
Tims Guynes <tims.m.guynes@gmail.com>
2022-10-25 17:22:51 -0700

    final commit for the day to ensure all changes are saved

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -17,7 +17,7 @@ label.pack(pady = 20)
 
 
 
-file_path = "D:\Projects\Python\data_conversion"
+file_path = "D:\Projects\Python\data_conversion\data"
 
 day_of_week = ["Monday",
                "Tuesday",
@@ -31,14 +31,15 @@ date = []
 
 def open_file():
     #open file from location selected
-    file = filedialog.askopenfilename(initialdir="D:\Projects\Python\data_conversion\data",
+    file = filedialog.askopenfilename(initialdir=file_path,
                                         title="Select a file", 
                                         filetypes=[("Text files","*.txt"), ("All files", "*.*")])
     ### original open filed
     f = open(file, encoding="utf8") #opens file from location and sets the encoding to prevent corruption
-    txt_file = ((str(f)[-41:-31]).replace("_","/")) #strips the date out of the file string
+    #txt_file = ((str(f)[-41:-31]).replace("_","/")) #strips the date out of the file string
+    txt_file = ((str(f)[-41:-31])) #strips the date out of the file string
     #set_date(txt_file)
-    convert_txt_to_csv(f)
+    convert_txt_to_csv(f, txt_file)
     print(txt_file)
     f.close()    
 
@@ -50,33 +51,31 @@ def set_date(date):
         pass
 '''
 
-def convert_txt_to_csv(file):
-    if exists("D:\Projects\Python\data_conversion\data\DSC.csv"):
+def convert_txt_to_csv(file, date_):
+    if exists(file_path + "\DSC"+date_+".csv"):
         pass
-        fix_file_format()
+        fix_file_format(date_)
     else:
         dataframe1 = pd.read_csv(file)
         print(dataframe1)
-        dataframe1.to_csv('DSC.csv', index = False)
-        fix_file_format()
+        dataframe1.to_csv('data\DSC' + str(date_)  + '.csv', index = False)
+        fix_file_format(date_)
 
 
-def fix_file_format():
-    
+def fix_file_format(date_):
     data = []
     list_new = []
 
-    with open('DSC.csv', newline='') as f:
+    with open('data\DSC'+date_+'.csv', newline='') as f:
         reader = csv.reader(f)
         data = list(reader)
         
     for d in data:
         new_list = []
         split_first = list(d[0].split(" "))
-        
         count = 0
+
         for l in split_first:
-            count += 1
             new_list.append(l)
             print(count)
             print(l)
