0d39d0d83d8a0d808e211b0ce3b304e060b62b29
Tims Guynes <tims.m.guynes@gmail.com>
2022-10-27 19:44:14 -0700

    working on adding data function, not finished

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -29,8 +29,8 @@ day_of_week = ["Monday",
                "Friday",
                "Saturday",
                "Sunday"]
-date_list = []
 
+date_list = []
 
 def open_file():
     #open file from location selected
@@ -45,11 +45,6 @@ def open_file():
     convert_txt_to_csv(f, txt_file)
     f.close()
 
-def date_strip(text_file):
-    txt_file = ((str(text_file)[-41:-31])) #strips the date out of the file string from the end of the file that spits out as per the open   
-    return txt_file    
-
-
 def set_date(date):
     new_date = datetime.datetime.strptime(date, "%m_%d_%Y")
     count = 0
@@ -58,10 +53,6 @@ def set_date(date):
         date_string = datetime.date.strftime(future_date, "%m/%d/%Y")
         date_list.append(str(date_string))
         count += 1
-        #print(str(date) + " this is the data coming in")
-        #print(date_string + " " + str(count))
-
-
 
 #original
 def convert_txt_to_csv(file, date_):
@@ -71,7 +62,7 @@ def convert_txt_to_csv(file, date_):
         # overwrite file with new data
     else:
         dataframe1 = pd.read_csv(file)
-        print(dataframe1)
+        #print(dataframe1)
         dataframe1.to_csv('data\DSC-' + str(date_)  + '.csv', index = False)
         fix_file_format(date_)
 
@@ -102,15 +93,94 @@ def fix_file_format(date_):
             new_list.append(l)#handlees the status and removes the spaces
 
         list_new.append(new_list)
+    add_data_to_list(list_new)
+
+
+def add_data_to_list(list):
     
-    #take 5 entries and put them in a list
-    #append list with missing data (days of week and dates)
-    #add OFF as the last item 
+    lst_loc = 0
+    tmp_list = []
+    #incom_lst = list[lst_loc-5:lst_loc]
+    
+    for count, tl in enumerate(list):
         
-    #print(count)
-    #print(temp_list)
-    #print(date_list)
-    #print(list_new[:5])
+        #print(tl)
+        # enumerate through the groups of 5 entries
+        # verify all days/dates are in (shoudl be a total lenght of 7
+        if count == lst_loc:
+            if tl[2] == "Wednesday":
+                pass
+            else:
+                #tmp_list.append(tl)
+                pass
+        if count == lst_loc + 1:
+            if tl[2] == "Tuesday":
+                tmp_list.append([tl[0], tl[1], "Monday", date_list[0], tl[4], "OFF"])
+        else:
+            #tmp_list += tl
+            pass
+        # Add to a temporary list
+        # sort list 
+        # add to main list to be converted
+        #print(lst_loc)
+        """
+        #print(count)
+        if count == 0:
+            tmp_list += incom_lst
+        #print(count < lst_loc)
+        #print(lst_loc < len(list))
+        if lst_loc < len(list):
+            #add missing information
+            #print(len(tmp_list[(lst_loc - 5):]))
+            #print(4 < len(tmp_list[(lst_loc - 5):]) < 7)
+            if 4 < len(tmp_list[(lst_loc - 5):]) < 7:
+                #print(tl[2])
+                if tl[2] != "Monday" and count == (lst_loc - 5):
+                    tmp_list.append([tl[0], tl[1], "Monday", date_list[0], tl[4], "OFF"])
+                    print(1)
+                    #print([tl[0], tl[1], day_of_week[0], date_list[0], tl[4], "OFF"])
+
+                if (tl[2] != "Tuesday" and count == (lst_loc - 4)) and (tl[2] != "Tuesday" and count == (lst_loc - 5)):                    
+                    tmp_list.append([tl[0], tl[1], "Tuesday", date_list[1], tl[4], "OFF"])
+                    print(2)
+
+                print(tl[2] != "Wednesday")
+                print(count == (lst_loc - 5))
+                if tl[2] != "Wednesday" and count == (lst_loc - 5):
+                    tmp_list.append([tl[0], tl[1], "Wednesday", date_list[2], tl[4], "OFF"])
+                    print(3)
+
+                if tl[2] != "Thursday" and count == (lst_loc - 4) and tl[2] != "Thursday" and count == (lst_loc - 3) and tl[2] != "Thursday" and count == (lst_loc - 2):
+                    tmp_list.append([tl[0], tl[1], "Thursday", date_list[3], tl[4], "OFF"])
+                    print(4)
+
+                if (tl[2] != "Friday" and count == (lst_loc - 3)) and (tl[2] != "Friday" and count == (lst_loc - 2)) and (tl[2] != "Friday" and count == (lst_loc - 1)):
+                    tmp_list.append([tl[0], tl[1], "Friday", date_list[4], tl[4], "OFF"])
+                    print(5)
+                if tl[2] != "Saturday" and count == (lst_loc - 2):
+                    if count == (lst_loc - 1):
+                        tmp_list.append([[tl[0], tl[1], "Saturday", date_list[5], tl[4], "OFF"]])
+                        print(6)
+                if tl[2] != "Sunday" and count == (lst_loc - 1) :
+                    tmp_list.append([tl[0], tl[1], "Sunday", date_list[6], tl[4], "OFF"])
+                    print(7)
+            print(count)
+        else:
+            break
+        
+        lst_loc += 5
+        #print(lst_loc)
+
+            
+                
+            #print(count, tl)"""
+    #print(tmp_list)
+    #print(enumerate(list))
+
+    
+    #print(tmp_list)
+    #print(len(list))
+    #print(tmp_list[(lst_loc-5):])
 
  
 button = ttk.Button(root, text="Open", command=open_file)
